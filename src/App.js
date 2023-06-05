ef85b86c6824c4e6ccf72a6ffb3246a8cad217d4
adithya3403 <a.dty.3403@gmail.com>
2023-06-05 16:40:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react';
+
+function sum(a, b) {
+    console.log('Calculating sum...');
+    return a + b;
+}
+
+function CalculateSum({ a, b }) {
+    const result = useMemo(() => {
+        return sum(a, b);
+    }, [a, b]);
+    return <div>{result}</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    function handleNum1Change(event) {
+        setNum1(Number(event.target.value));
+    }
+    function handleNum2Change(event) {
+        setNum2(Number(event.target.value));
+    }
+    return (
+        <div>
+            <label>
+                Number 1 : <br></br>
+                <input type="number" value={num1} onChange={handleNum1Change} />
+            </label>
+            <br></br>
+            <label>
+                Number 2 : <br></br>
+                <input type="number" value={num2} onChange={handleNum2Change} />
+            </label>
+            <br></br>Sum:<CalculateSum a={num1} b={num2} />
+        </div>
+    );
 }
 
 export default App;
